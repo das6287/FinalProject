a5db53ffc7d31261f795e0cd34ee58b217b530ef
Joshua Haines <jfh5656@psu.edu>
2020-04-30 07:43:03 -0700

    Added methods to DBInteract that calculate growth rate of the virus in each country and growth rate of deaths both worldwide and in each country

--- FinalProject2/src/Model/DBInteract.java
+++ FinalProject2/src/Model/DBInteract.java
@@ -121,6 +121,65 @@ public class DBInteract {
     }
     
     /**
+     * Calculates worldwide growth rate of deaths from coronavirus from previous day
+     * 
+     * @param date Date for the data
+     * @return Percentage growth of deaths from previous day
+     */
+    public static double getWorldDeathRate(String date) {
+        
+        double percentIncrease = -1;
+        
+        try {
+            Connection con = DriverManager.getConnection(url,user,password);  
+
+            Statement stmt = con.createStatement();
+            
+            double dateDeaths = -1;
+            double previousDeaths = -1;
+            
+            String previousDate = getPreviousDate(date);
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM CoronavirusWorld " +
+                "WHERE Date = '" + date + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Data not found for: " + date);
+                     return -1;
+            } else {
+               dateDeaths = rs.getInt(4);
+            }
+            
+            rs = stmt.executeQuery("SELECT * FROM CoronavirusWorld " +
+                "WHERE Date = '" + previousDate + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Data not found for: " + date);
+                     return -1;
+            } else {
+               previousDeaths = rs.getInt(4);     
+            }
+            
+            if(previousDeaths == 0) {
+                System.out.println("Growth rate undefined");
+                return -1;
+            }
+            
+            percentIncrease = ((dateDeaths/previousDeaths - 1) * 100);
+      
+            stmt.close();
+
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println("catch5");
+            System.out.println(e);
+        }
+        
+        return percentIncrease;
+    }
+    
+    /**
      * Gets data for a specified country's coronavirus cases, recoveries, and 
      * deaths, from the CoronavirusCountries table
      * 
@@ -160,58 +219,161 @@ public class DBInteract {
         return -1;
     }
     
-//    /**
-//     * Calculates growth rate of coronavirus in each country frmo previous day
-//     * 
-//     * @param date Date for the data
-//     * @param country Country for the data
-//     * @return Percentage growth of coronavirus from previous day in specified country
-//     */
-//    public static double getCountryGrowthRate(String date, String country) {
-//        
-//        try {
-//            Connection con = DriverManager.getConnection(url,user,password);  
-//
-//            Statement stmt = con.createStatement();
-//            
-//            int dateCases = -1;
-//            int previousCases = -1;
-//            
-//            String [] dateArray = date.split("/");
-//            String previousDate;
-//            
-//            if(dateArray.)
-//            
-//
-//            ResultSet rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
-//                "WHERE Date = '" + date + "' AND Country = '" + country + "'");
-//            
-//            if (rs.next() == false) { 
-//                     System.out.println("Data not found for: " + country +
-//                        " on " +  date);
-//            } else {
-//               dateCases = rs.getInt(3);       
-//            }
-//            
-//            rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
-//                "WHERE Date = '" + date + "' AND Country = '" + country + "'");
-//            
-//            if (rs.next() == false) { 
-//                     System.out.println("Data not found for: " + country +
-//                        " on " +  date);
-//            } else {
-//               dateCases = rs.getInt(3);       
-//            }
-//
-//            stmt.close();
-//
-//            con.close();
-//        }
-//        catch (Exception e) {
-//            System.out.println("catch4");
-//            System.out.println(e);
-//        }
-//        
-//        return -1;
-//    }
+    /**
+     * Calculates growth rate of coronavirus in each country from previous day
+     * 
+     * @param date Date for the data
+     * @param country Country for the data
+     * @return Percentage growth of coronavirus from previous day in specified country
+     */
+    public static double getCountryGrowthRate(String date, String country) {
+        
+        double percentIncrease = -1;
+        
+        try {
+            Connection con = DriverManager.getConnection(url,user,password);  
+
+            Statement stmt = con.createStatement();
+            
+            double dateCases = -1;
+            double previousCases = -1;
+            
+            String previousDate = getPreviousDate(date);
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
+                "WHERE Date = '" + date + "' AND Country = '" + country + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Data not found for: " + country +
+                        " on " +  date);
+                     return -1;
+            } else {
+               dateCases = rs.getInt(3);       
+            }
+            
+            rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
+                "WHERE Date = '" + previousDate + "' AND Country = '" + country + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Previous date not found for: " + country +
+                        " on " +  date);
+                     return -1;
+            } else {
+               previousCases = rs.getInt(3);     
+            }
+            
+            if(previousCases == 0) {
+                return -1;
+            }
+            
+            percentIncrease = ((dateCases/previousCases - 1) * 100);
+      
+            stmt.close();
+
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println("catch5");
+            System.out.println(e);
+        }
+        
+        return percentIncrease;
+    }
+    
+    /**
+     * Calculates growth rate of deaths from coronavirus in each country from previous day
+     * 
+     * @param date Date for the data
+     * @param country Country for the data
+     * @return Percentage growth of coronavirus from previous day in specified country
+     */
+    public static double getCountryDeathRate(String date, String country) {
+        
+        double percentIncrease = -1;
+        
+        try {
+            Connection con = DriverManager.getConnection(url,user,password);  
+
+            Statement stmt = con.createStatement();
+            
+            double dateDeaths = -1;
+            double previousDeaths = -1;
+            
+            String previousDate = getPreviousDate(date);
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
+                "WHERE Date = '" + date + "' AND Country = '" + country + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Data not found for: " + country +
+                        " on " +  date);
+                     return -1;
+            } else {
+               dateDeaths = rs.getInt(5);       
+            }
+            
+            rs = stmt.executeQuery("SELECT * FROM CoronavirusCountries " +
+                "WHERE Date = '" + previousDate + "' AND Country = '" + country + "'");
+            
+            if (rs.next() == false) { 
+                     System.out.println("Previous date not found for: " + country +
+                        " on " +  date);
+                     return -1;
+            } else {
+               previousDeaths = rs.getInt(5);     
+            }
+            
+            if(previousDeaths == 0) {
+                return -1;
+            }
+            
+            percentIncrease = ((dateDeaths/previousDeaths - 1) * 100);
+      
+            stmt.close();
+
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println("catch5");
+            System.out.println(e);
+        }
+        
+        return percentIncrease;
+    }
+    
+    /**
+     * Determines previous date from an inputted date
+     * 
+     * @param date Inputted date
+     * @return Previous date, in string form
+     */
+    private static String getPreviousDate(String date) {
+        
+        String [] dateArray = date.split("/");
+        
+        int month = Integer.parseInt(dateArray[0]);
+        int day = Integer.parseInt(dateArray[1]);
+        int year = Integer.parseInt(dateArray[2]);
+        String previousDate;
+
+        if(day > 1) {
+            previousDate = (dateArray[0] + "/" + Integer.toString(day - 1) +
+                    "/" + dateArray[2]);
+        } else if (month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11) {
+            previousDate = (Integer.toString(month - 1) + "/31/" + dateArray[2]);
+        } else if (month == 5 || month == 7 || month == 10 || month == 12) {
+            previousDate = (Integer.toString(month - 1) + "/30/" + dateArray[2]);
+        } else if (month == 3) {
+            if (year % 4 == 0) {
+                previousDate = ("2/29/" + dateArray[2]);
+            } else {
+                previousDate = ("2/28/" + dateArray[2]);
+            }
+        } else {
+            previousDate = ("12/31/" + Integer.toString(year - 1));
+        }
+        
+        return previousDate;
+    }
+
 }
--- FinalProject2/src/Model/DBInteractTest.java
+++ FinalProject2/src/Model/DBInteractTest.java
@@ -22,6 +22,9 @@ public class DBInteractTest {
         System.out.println(DBInteract.getWorldGrowthRate("4/15/2020"));
         System.out.println(DBInteract.getWorldData("4/21/2020", 0));
         System.out.println(DBInteract.getCountryData("4/15/2020", "US", 0));
+        System.out.println(DBInteract.getCountryGrowthRate("1/23/2020", "China"));
+        System.out.println(DBInteract.getWorldDeathRate("4/15/2020"));
+        System.out.println(DBInteract.getCountryDeathRate("4/1/2020","US"));
     }
     
 }
